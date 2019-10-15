f7cd8d472ddebc0a51d458077191955a4010d3f3
ZaferZ <50319411+ZaferZ@users.noreply.github.com>
2019-10-15 15:09:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zadaca8_1._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if(a>10 && a<200)
+            {
+                for(int i=a;i>1;i--)
+                {
+                    if (i % 7 == 0)
+                    {
+                        Console.WriteLine(i);
+                       
+                    }
+                }
+      
+            }
+        }
+    }
+}
